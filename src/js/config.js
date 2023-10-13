39f3deedf75b000eeb25e050090494e9d05bc9b0
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-10-13 20:13:04 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RESULT_PER_PAGE = 10;
+export const KEY = 'ff021079-9618-45a2-b9b3-198d2ea47e0c';
+export const MODAL_CLOSE_SEC = 2.5;
