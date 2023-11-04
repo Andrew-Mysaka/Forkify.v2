30ce89746295412be1d1797a62d67ee4993cf3da
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-04 21:11:00 +0200

    fixing URL to work on Netlify

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RESULT_PER_PAGE = 10;
 export const KEY = 'ff021079-9618-45a2-b9b3-198d2ea47e0c';
